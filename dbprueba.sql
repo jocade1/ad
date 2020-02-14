763119e98c3fef78b4e419fd37f34fd3bb8c4fc2
josdel1 <josealberto1407@gmail.com>
2020-02-14 09:19:47 +0100

    Base de datos del proyecto final

--- dbprueba.sql
+++ dbprueba.sql
@@ -1,22 +1,219 @@
-create table categoria(
-    id serial primary key,
-    nombre varchar(50) not null unique
-);
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 14-02-2020 a las 00:17:31
+-- Versión del servidor: 8.0.17
+-- Versión de PHP: 7.3.9
 
-insert into categoria (nombre) values ( 'categoria 1 ');
-insert into categoria (nombre) values ( 'categoria 2 ');
-insert into categoria (nombre) values ( 'categoria 3 ');
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-create table articulo(
-    id serial primary key,
-    nombre varchar(50) not null unique,
-    precio decimal(10,2), categoria unsigned bigint references categoria(id),
-    categoria bigint unsigned,
-    foreign key (categoria) references|
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-insert into categoria () values ('');
-insert into categoria () values ('');
-insert into categoria () values ('');
-insert into categoria () values ('');
+--
+-- Base de datos: `dbprueba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `articulo`
+--
+
+CREATE TABLE `articulo` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `precio` double(10,2) UNSIGNED NOT NULL,
+  `categoria` bigint(20) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `articulo`
+--
+
+INSERT INTO `articulo` (`id`, `nombre`, `precio`, `categoria`) VALUES
+(1, 'Mesa', 15.00, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id`, `nombre`) VALUES
+(6, 'cat 5'),
+(1, 'categoria 1'),
+(2, 'categoria 2'),
+(3, 'categoria 3'),
+(4, 'categoria 4'),
+(17, 'Herramienta'),
+(16, 'Herramientas'),
+(13, 'Soporter'),
+(18, 'Yo que se');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id`, `nombre`) VALUES
+(6, 'BElen'),
+(4, 'Javier'),
+(3, 'jose alberto'),
+(2, 'ruben'),
+(11, 'Tamara');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `fecha` datetime NOT NULL,
+  `cliente` bigint(20) UNSIGNED NOT NULL,
+  `importe` float(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `pedido`
+--
+
+INSERT INTO `pedido` (`id`, `fecha`, `cliente`, `importe`) VALUES
+(4, '2020-02-10 09:58:46', 6, 350.00),
+(5, '2020-02-10 10:36:23', 4, 50.00),
+(7, '2020-02-10 10:39:59', 3, 0.00),
+(9, '2020-02-10 10:43:26', 3, 300.00);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidolinea`
+--
+
+CREATE TABLE `pedidolinea` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `pedido` bigint(20) UNSIGNED NOT NULL,
+  `articulo` bigint(20) UNSIGNED NOT NULL,
+  `precio` float(10,2) NOT NULL,
+  `unidades` float(10,2) NOT NULL,
+  `importe` float(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `pedidolinea`
+--
+
+INSERT INTO `pedidolinea` (`id`, `pedido`, `articulo`, `precio`, `unidades`, `importe`) VALUES
+(3, 4, 1, 15.00, 20.00, 300.00),
+(4, 7, 1, 15.00, 20.00, 300.00),
+(6, 9, 1, 15.00, 20.00, 300.00);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- Indices de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `fecha` (`fecha`);
+
+--
+-- Indices de la tabla `pedidolinea`
+--
+ALTER TABLE `pedidolinea`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `pedidolinea`
+--
+ALTER TABLE `pedidolinea`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
